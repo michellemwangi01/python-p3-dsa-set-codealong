b3f737ed28715ea2536f6af599ee4fe6f696104a
michellemwangi01 <michellemwangim@gmail.com>
2023-09-03 21:46:29 +0300

    completed

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,26 @@
 class MySet:
+    def __init__(self, your_list = []):
+        self.my_list = your_list
+        self.dictionary = {}
+        for value in self.my_list:
+            self.dictionary[value] = True
 
-    pass
+    def __repr__(self):
+        return f'{[key for key in self.dictionary.keys()]}'
+
+    def has(self, value):
+        return value in self.dictionary
+
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+
+    def delete(self,value):
+        return self.dictionary.pop(value, None)
+
+    def size(self):
+        return len(self.dictionary)
+
+    def clear(self):
+        return self.dictionary.clear()
+
